5055dc55deeb2b2cffdd443121469576e309a9f2
sethts <>
2008-03-19 01:02:55 +0000

    Lesson learned

--- FileManagement/__init__.py
+++ FileManagement/__init__.py
@@ -23,7 +23,7 @@ def get_extension(path):
     return ""
 
 def clear_string(strng):
-    r_chars = '_.,()'
+    r_chars = string.punctuation
     return strng.translate(string.maketrans(r_chars," "*len(r_chars))).replace(" ", "")
     
 def without_extension(filename):
