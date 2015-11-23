5d6b60802d43f0aa2eefc226109b9243bee1bb9c
Anonymous Maarten <anonymous.maarten@gmail.com>
2015-11-23 23:47:50 +0100

    run.py: format sources using autopep8

--- run.py
+++ run.py
@@ -2,18 +2,20 @@
 # Copyright (c) 2015 SubDownloader Developers - See COPYING - GPLv3
 # PYTHON_ARGCOMPLETE_OK
 
-import sys, os, platform
+import sys
+import os
+import platform
 
-#calculating the folder of SubDownloader src (or .exe)
+# calculating the folder of SubDownloader src (or .exe)
 if os.path.isfile(sys.path[0]):
-        subdownloader_folder = os.path.dirname(sys.path[0])
+    subdownloader_folder = os.path.dirname(sys.path[0])
 else:
-        subdownloader_folder = sys.path[0]
+    subdownloader_folder = sys.path[0]
 
 import logging
 import argparse
 
-sys.path.append(os.path.join(sys.path[0], 'modules') )
+sys.path.append(os.path.join(sys.path[0], 'modules'))
 
 import modules.configuration as conf
 
@@ -21,24 +23,24 @@ parser = argparse.ArgumentParser(description=conf.General.description)
 conf.Terminal.populate_parser(parser)
 
 try:
-  import argcomplete
-  argcomplete.autocomplete(parser)
+    import argcomplete
+    argcomplete.autocomplete(parser)
 except ImportError:
-  pass
+    pass
 
 options = parser.parse_args()
 
 if platform.system() == "Windows":
     try:
         sys.stderr = open(os.path.join(subdownloader_folder,
-            "subdownloader.log"), "w")
-        #The EXE file in windows will think that outputs here are errors,
-        #and it will show annoying mesage about run.exe.log
+                                       "subdownloader.log"), "w")
+        # The EXE file in windows will think that outputs here are errors,
+        # and it will show annoying mesage about run.exe.log
     except:
         pass
-        #Cannot write message into subdownloader.log,
-        #that happens for example in Vista,
-        #where SD does not have writer permission on its ProgramFiles folder.
+        # Cannot write message into subdownloader.log,
+        # that happens for example in Vista,
+        # where SD does not have writer permission on its ProgramFiles folder.
 
 logging.basicConfig(level=options.logging,
                     format=conf.Logging.log_format,
@@ -78,4 +80,3 @@ if __name__ == "__main__":
     elif options.mode == 'cli':
         cli = cli.main.Main(options)
         cli.start_session()
-
