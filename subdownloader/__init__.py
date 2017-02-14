25697cd6858a2c77002ca228d3f41b84f047f595
Anonymous Maarten <anonymous.maarten@gmail.com>
2017-02-14 01:48:41 +0100

    Add WEBSITE to subdownloader/__init__.py

--- subdownloader/__init__.py
+++ subdownloader/__init__.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2015 SubDownloader Developers - See COPYING - GPLv3
+# Copyright (c) 2017 SubDownloader Developers - See COPYING - GPLv3
 
-'''
-MODULES package
-'''
+"""
+The subdownloader package provides an interface to download/upload subtitles for media files.
+"""
 
 APP_TITLE = "SubDownloader"
 APP_VERSION = "2.0.19"
+WEBSITE = 'https://github.com/subdownloader/subdownloader'
