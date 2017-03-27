bd82dd8a76f90e9e53cef77f6afe5dd07ddad624
Anonymous Maarten <anonymous.maarten@gmail.com>
2017-03-27 23:51:42 +0200

    gui/languageCombobox: don't import language module

--- subdownloader/client/gui/languageComboBox.py
+++ subdownloader/client/gui/languageComboBox.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2017 SubDownloader Developers - See COPYING - GPLv3
 
-from subdownloader.languages import language
+from subdownloader.languages.language import Language, legal_languages, UnknownLanguage
 from subdownloader.client.internationalization import i18n_get_supported_locales
 
 from PyQt5.QtCore import pyqtSignal, pyqtSlot, Qt
@@ -10,11 +10,11 @@ from PyQt5.QtWidgets import QComboBox
 
 class AbstractLanguageComboBox(QComboBox):
 
-    selected_language_changed = pyqtSignal(language.Language)
+    selected_language_changed = pyqtSignal(Language)
 
     def __init__(self, parent=None):
         QComboBox.__init__(self, parent)
-        self._unknown_text = language.UnknownLanguage.create_generic().name()
+        self._unknown_text = UnknownLanguage.create_generic().name()
         self._unknown_visible = True
         self._current_language = None
 
@@ -34,7 +34,7 @@ class AbstractLanguageComboBox(QComboBox):
 
     def setupUi(self):
         if self._unknown_visible:
-            self.addItem(self._unknown_text, language.UnknownLanguage.create_generic().raw_data())
+            self.addItem(self._unknown_text, UnknownLanguage.create_generic().raw_data())
 
         languages = sorted(self.legal_languages(), key=lambda x:x.name())
 
@@ -51,20 +51,20 @@ class AbstractLanguageComboBox(QComboBox):
         self.currentIndexChanged.connect(self.onCurrentIndexChanged)
 
         self.setCurrentIndex(0)
-        self._current_language = language.Language.from_raw_data(self.itemData(0, Qt.UserRole))
+        self._current_language = Language.from_raw_data(self.itemData(0, Qt.UserRole))
 
     @pyqtSlot(int)
     def onCurrentIndexChanged(self, index):
         raw_data = self.itemData(index, Qt.UserRole)
-        self._current_language = language.Language.from_raw_data(raw_data)
+        self._current_language = Language.from_raw_data(raw_data)
         self.selected_language_changed.emit(self._current_language)
 
 
 class LanguageComboBox(AbstractLanguageComboBox):
     def legal_languages(self):
-        return language.legal_languages()
+        return legal_languages()
 
 
 class InterfaceLanguageComboBox(AbstractLanguageComboBox):
     def legal_languages(self):
-        return [language.Language.from_locale(locale) for locale in i18n_get_supported_locales()]
+        return [Language.from_locale(locale) for locale in i18n_get_supported_locales()]
