b3c5e42dffc242f81afc7927a3fde4c15ee92afb
Anonymous Maarten <anonymous.maarten@gmail.com>
2018-02-28 20:54:03 +0100

    metadata: remove kaa-metadata + rework metadata detection

--- subdownloader/metadata.py
+++ subdownloader/metadata.py
@@ -43,9 +43,9 @@ class Metadata(object):
         """
         self._video_tracks = []
 
-    def _add_metadata(self, metadata):
+    def add_metadata(self, metadata):
         """
-        Private method to add a new MetadataVideoTrack instance
+        Add a new MetadataVideoTrack instance
         :param metadata: metadata of a video track
         """
         self._video_tracks.append(metadata)
@@ -57,37 +57,36 @@ class Metadata(object):
         """
         return self._video_tracks
 
-    def _parse_dummy(self, path):
-        """
-        Private function to pretend to parse video at filepath, but don't do anything actually.
-        :param path: path of video to parse as string
-        """
-        pass
 
-    def _parse_kaa_metadata(self, path):
-        """
-        Private function to parse video at filepath, using kaa framework.
-        :param path: path of video to parse as string
-        """
-        log.debug('kaamediainfo: parsing "{path}" ...'.format(path=path))
-        parseRes = kaa.metadata.parse(str(path))
-        if not parseRes:
-            return
-        for video in parseRes.video:
-            log.debug('... found video track')
-            self._add_metadata(
-                MetadataVideoTrack(
-                    duration_ms=1000 * parseRes.length,
-                    framerate=video.fpsm,
-                    framecount=None
-                )
-            )
+class PyMediaInfoParser(object):
+    def __init__(self):
+        if not self.is_available():
+            raise RuntimeError('Cannot create PyMediaInfoParser')
 
-    def _parse_pymediainfo(self, path):
+    @staticmethod
+    def is_available():
+        try:
+            log.debug('Trying PyMediaInfoParser ...')
+            import pymediainfo
+            if pymediainfo.MediaInfo.can_parse():
+                log.debug('... Succeeded')
+                return True
+            else:
+                log.debug('... Failed! (can_parse failed)')
+                return False
+        except ImportError:
+            log.debug('... Failed! (ImportError)')
+            return False
+
+    @staticmethod
+    def parse_path(path):
         """
-        Private function to parse video at filepath, using pymediainfo framework.
+        Parse a video at filepath, using pymediainfo framework.
         :param path: path of video to parse as string
         """
+        import pymediainfo
+
+        metadata = Metadata()
         log.debug('pymediainfo: parsing "{path}" ...'.format(path=path))
         parseRes = pymediainfo.MediaInfo.parse(str(path))
         log.debug('... parsing FINISHED')
@@ -103,52 +102,30 @@ class Metadata(object):
                     continue
                 log.debug('... duration = {duration_ms} ms, framerate = {framerate} fps'.format(duration_ms=duration_ms,
                                                                                                framerate=framerate))
-                self._add_metadata(
+                metadata.add_metadata(
                     MetadataVideoTrack(
                         duration_ms=duration_ms,
                         framerate=float(framerate),
                         framecount=framecount
                     )
                 )
+        return metadata
 
 parser = None
 
 log.debug('Importing metadata parsing module ...')
 
-if not parser:
-    try:
-        log.debug('Trying kaa.metadata ...')
-        import kaa.metadata
-        log.debug('... Succeeded')
-
-        # Not interested in any output of the metadata package
-        logging.getLogger('metadata').setLevel(logging.ERROR)
-
-        log.debug('parsing module = kaa.metadata')
-        parser = Metadata._parse_kaa_metadata
-    except ImportError:
-        log.debug('... Failed!')
+if PyMediaInfoParser.is_available():
+    parser = PyMediaInfoParser()
 
 if not parser:
-    try:
-        log.debug('Trying pymediainfo ...')
-        import pymediainfo
-        if pymediainfo.MediaInfo.can_parse:
-            log.debug('... Succeeded')
-            log.debug('parsing module = pymediainfo')
-            parser = Metadata._parse_pymediainfo
-        else:
-            log.debug('... Failed!')
-    except ImportError:
-        log.debug('... Failed!')
-
-if not parser:
-    log.debug('parsing module = dummy parser')
-    parser = Metadata._parse_dummy
-
-Metadata.parse = parser
+    log.debug('No parsing module found')
 
 log.debug('... Importing metadata parsing module finished')
+
+
+def available():
+    return parser is not None
 
 
 def parse(path):
@@ -157,6 +134,4 @@ def parse(path):
     :param path: path of video to parse as string
     :return: Metadata Instance of the video
     """
-    metadata = Metadata()
-    metadata.parse(path)
-    return metadata
+    return parser.parse_path(path)
