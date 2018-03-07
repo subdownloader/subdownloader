429846e8e20702bb4860d695eecae936f1a770a1
Anonymous Maarten <anonymous.maarten@gmail.com>
2018-03-07 14:28:00 +0100

    project: drop support for python 2.7 and 3.4 in setup.py

--- setup.py
+++ setup.py
@@ -57,8 +57,6 @@ setup(
         "Intended Audience :: End Users/Desktop",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Natural Language :: English",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
     ],
