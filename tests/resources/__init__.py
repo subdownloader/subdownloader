85b9c7f71d4fa41a0dee1528ec217ac024dc0103
Anonymous Maarten <anonymous.maarten@gmail.com>
2018-03-22 02:24:22 +0100

    test: add breakdance.avi to version control

--- tests/resources/__init__.py
+++ tests/resources/__init__.py
@@ -16,15 +16,6 @@ def resources_init():
     RESOURCE_PATH.mkdir(exist_ok=True)
     RESOURCE_CHILD_PATH.mkdir(exist_ok=True)
 
-    if not RESOURCE_AVI.exists():
-        URL = 'http://www.opensubtitles.org/addons/avi/{}'
-        urlretrieve(url=URL.format('breakdance.avi'),
-                    filename=str(RESOURCE_AVI))
-
-    if not RESOURCE_AVI.exists():
-        raise RuntimeError('Could not download resource')
-
-    with RESOURCE_AVI.open('rb') as f:
-        md5_hash = md5(f.read()).hexdigest()
+    md5_hash = md5(RESOURCE_AVI.read_bytes()).hexdigest()
     if md5_hash != 'dc6cc911a5f71b39918f9b24b73b8f26':
         raise RuntimeError('Resource corrupt: md5={}'.format(md5_hash))
