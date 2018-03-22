6ca5abea0b1575c5a35a51dd537f2afdcf633ab1
Anonymous Maarten <anonymous.maarten@gmail.com>
2018-03-22 19:01:44 +0100

    setup: change quotes + more urls

--- setup.py
+++ setup.py
@@ -18,16 +18,20 @@ def read(filename):
     return (project_path / filename).read_text()
 
 install_requires = [
-    "argparse >= 1.3.0",
-    "argcomplete >= 1.7.0",
-    "langdetect >= 1.0.7",
-    "pymediainfo >= 2.1.6",
-    "PyQt5 >= 5.0.0",
+    'argparse >= 1.3.0',
+    'argcomplete >= 1.7.0',
+    'langdetect >= 1.0.7',
+    'pymediainfo >= 2.1.6',
+    'PyQt5 >= 5.0.0',
 ]
 
 setup_requires = [
-    "sphinx-argparse >= 0.2.1",
-    "docutils >= 0.14",
+    'sphinx-argparse >= 0.2.1',
+    'docutils >= 0.14',
+]
+
+tests_requires = [
+    'pytest >= 3.4',
 ]
 
 setup(
@@ -37,33 +41,36 @@ setup(
     author_email=subdownloader.project.PROJECT_MAINTAINER_MAIL,
     maintainer_email=subdownloader.project.PROJECT_MAINTAINER_MAIL,
     description=subdownloader.project.get_description(),
-    keywords="download upload automatic subtitle download movie video film search",
-    url=subdownloader.project.WEBSITE_MAIN,
-    license="GPL3",
+    keywords=('download', 'upload', 'automatic', 'subtitle', 'movie', 'video', 'film', 'search'),
+    license='GPL3',
     packages=find_packages(exclude=('tests', 'tests.*', )),
     long_description=read('README.md'),
     long_description_content_type='text/markdown',
+    url=subdownloader.project.WEBSITE_MAIN,
+    download_url=subdownloader.project.WEBSITE_RELEASES,
     project_urls={
-        'source': subdownloader.project.WEBSITE_MAIN,
-        'tracker': subdownloader.project.WEBSITE_ISSUES,
+        'Source Code': subdownloader.project.WEBSITE_MAIN,
+        'Bug Tracker': subdownloader.project.WEBSITE_ISSUES,
+        'Translations': subdownloader.project.WEBSITE_TRANSLATE,
     },
     classifiers=[
-        "Topic :: Multimedia",
-        "Topic :: Utilities",
-        "Environment :: Console",
-        "Environment :: X11 Applications :: Qt",
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: End Users/Desktop",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Natural Language :: English",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
+        'Topic :: Multimedia',
+        'Topic :: Utilities',
+        'Environment :: Console',
+        'Environment :: X11 Applications :: Qt',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     install_requires=install_requires,
     setup_requires=setup_requires,
+    tests_require=tests_requires,
     entry_points={
-        "console_scripts": [
-            "subdownloader = subdownloader.client.__main__:main"
+        'console_scripts': [
+            'subdownloader = subdownloader.client.__main__:main'
         ],
     },
     include_package_data=True,
