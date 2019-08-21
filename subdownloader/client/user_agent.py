0df16ebbdff3cd4895b6e4c2034193f02c2062ce
Anonymous Maarten <anonymous.maarten@gmail.com>
2019-08-21 17:48:00 +0200

    client: Send full version string to sites

--- subdownloader/client/user_agent.py
+++ subdownloader/client/user_agent.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2019 SubDownloader Developers - See COPYING - GPLv3
 
-from subdownloader.project import PROJECT_TITLE, PROJECT_VERSION_STR
+from subdownloader.project import PROJECT_TITLE, PROJECT_VERSION_FULL_STR
 
 from subdownloader.provider import opensubtitles
 
 
 def user_agent_init():
-    opensubtitles.set_default_user_agent('{title} {version}'.format(title=PROJECT_TITLE, version=PROJECT_VERSION_STR))
+    opensubtitles.set_default_user_agent('{title} {version}'.format(title=PROJECT_TITLE, version=PROJECT_VERSION_FULL_STR))
+    
