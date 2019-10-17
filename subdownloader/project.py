62960412bee98956266092753516c865f9f650d3
Anonymous Maarten <anonymous.maarten@gmail.com>
2019-10-17 19:24:13 +0200

    project: Release 2.1.0

--- subdownloader/project.py
+++ subdownloader/project.py
@@ -17,7 +17,7 @@ PROJECT_VERSION = (2, 1, 0)
 """
 Full version of the project as a tuple.
 """
-PROJECT_VERSION_FULL = PROJECT_VERSION + ('rc4', )
+PROJECT_VERSION_FULL = PROJECT_VERSION + ('', )
 
 """
 Version of the project as a string.
